037739b267f7613442bdd4cb18a7d052a1acc7ae
Thiago Grabe <tmgmeireles@hotmail.com>
2024-03-10 22:54:55 -0300

    feat: Exercise 14 Ok

--- lesson-5-final-pipeline-release-and-deploy/exercises/exercise_14/solution/evaluate/run.py
+++ lesson-5-final-pipeline-release-and-deploy/exercises/exercise_14/solution/evaluate/run.py
@@ -6,7 +6,7 @@ import pandas as pd
 import wandb
 import mlflow.sklearn
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, plot_confusion_matrix
+from sklearn.metrics import roc_auc_score, confusion_matrix, ConfusionMatrixDisplay
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -40,15 +40,27 @@ def go(args):
 
     logger.info("Computing confusion matrix")
     fig_cm, sub_cm = plt.subplots(figsize=(10, 10))
-    plot_confusion_matrix(
-        pipe,
-        X_test[used_columns],
-        y_test,
+
+    y_pred = pipe.predict(X_test)
+
+    cm = confusion_matrix(
+                y_true=y_test,
+                y_pred=y_pred,
+                labels=pipe["classifier"].classes_,
+                normalize="true"
+            )
+
+    disp  = ConfusionMatrixDisplay(
+                    confusion_matrix=cm,
+                    display_labels=pipe["classifier"].classes_
+                )
+
+    disp.plot(
         ax=sub_cm,
-        normalize="true",
         values_format=".1f",
         xticks_rotation=90,
     )
+
     fig_cm.tight_layout()
 
     run.log(
--- lesson-5-final-pipeline-release-and-deploy/exercises/exercise_14/solution/main.py
+++ lesson-5-final-pipeline-release-and-deploy/exercises/exercise_14/solution/main.py
@@ -1,12 +1,19 @@
 import mlflow
 import os
 import hydra
+import wandb
 from omegaconf import DictConfig, OmegaConf
 
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
 def go(config: DictConfig):
+
+    wandb.config = OmegaConf.to_container(
+         config, 
+         resolve=True, 
+         throw_on_missing=True
+    )
 
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
